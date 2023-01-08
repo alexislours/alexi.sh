de5c412524d47a9e7b683b4abcb04d04e8df4460
Alexis LOURS <alexislours@protonmail.com>
2023-01-08 23:39:39 +0100

    feat: add remark-toc

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -4,13 +4,14 @@ import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeImgSize from 'rehype-img-size'
 import supersub from 'remark-supersub';
+import remarkToc from 'remark-toc';
 
 export default {
 	extensions: ['.md'],
 	smartypants: {
 		dashes: 'oldschool'
 	},
-	remarkPlugins: [relativeImages, supersub],
+	remarkPlugins: [relativeImages, supersub, remarkToc],
 	rehypePlugins: [
 		[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
 		rehypeSlug,
