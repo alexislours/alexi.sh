971c24459b8f2ea5654f9b3754a50ae56205042a
Alexis LOURS <alexislours@protonmail.com>
2023-01-08 16:35:30 +0100

    feat: add page transitions

---
+++ src/lib/components/layout/Transition.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+	export let pathname = '';
+</script>
+
+{#key pathname}
+	<div in:fly={{ x: -5, duration: 300, delay: 0 }}>
+		<slot />
+	</div>
+{/key}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,19 @@
     import "../app.css";
     import Header from "$lib/components/layout/Header.svelte";
     import Footer from "$lib/components/layout/Footer.svelte";
+    import Transition from "$lib/components/layout/Transition.svelte";
+
+    export let data: { pathname: string };
 </script>
 
 <div class="mx-auto max-w-3xl px-4 sm:px-6">
     <div class="flex h-screen flex-col">
         <Header />
         <main>
-            <slot />
+            <Transition pathname={data?.pathname}>
+                <slot />
+            </Transition>
         </main>
-        <Footer/>
+        <Footer />
     </div>
 </div>
