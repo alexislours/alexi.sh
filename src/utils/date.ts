2713c73beadcce48aa9c8bf8ffd1c55311118289
Alexis LOURS <alexislours@protonmail.com>
2023-01-09 02:20:36 +0100

    content: add macOS apps post

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -5,4 +5,25 @@ const formatProjectDate = (date: string) => {
     return `${month} ${year}`;
 };
 
-export { formatProjectDate }
+const timeSince = (date: string) => {
+    const seconds = Math.floor((new Date().getTime() - new Date(date).getTime()) / 1000);
+  
+    const intervals = [
+      { label: 'year', value: 31536000 },
+      { label: 'month', value: 2592000 },
+      { label: 'day', value: 86400 },
+      { label: 'hour', value: 3600 },
+      { label: 'minute', value: 60 },
+      { label: 'second', value: 1 }
+    ];
+  
+    for (const { label, value } of intervals) {
+      const interval = seconds / value;
+      if (interval > 0) {
+        return `${Math.floor(interval)} ${label}${interval > 1 ? 's' : ''}`;
+      }
+    }
+  };
+  
+
+export { formatProjectDate, timeSince }
