507f2be2fb572921b6db3874e9c2582e1bba18e3
Alexis LOURS <alexislours@protonmail.com>
2023-01-09 15:28:21 +0100

    feat: add page loader

--- src/lib/components/layout/Transition.svelte
+++ src/lib/components/layout/Transition.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
+	import { fade } from 'svelte/transition';
 	export let pathname = '';
 </script>
 
 {#key pathname}
-	<div in:fly={{ x: -5, duration: 300, delay: 0 }}>
+	<div in:fade={{ duration: 300 }}>
 		<slot />
 	</div>
 {/key}
