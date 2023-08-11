3e897657110769ac52b9bdd4b80230b89e8e82dc
Alexis LOURS <alexislours@protonmail.com>
2023-08-12 00:47:19 +0200

    feat: add mobile sidebar

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,7 @@
 export const SITE_TITLE = "Alexis LOURS";
 export const SITE_DESCRIPTION = "Full-Stack developer from France.";
+export const SIDEBAR_LINKS: { title: string; href: string }[] = [
+  { title: "Home", href: "/" },
+  { title: "Blog", href: "/blog" },
+  { title: "Projects", href: "/projects" },
+];
