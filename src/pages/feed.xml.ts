315ed5f79bc19eeec1c42e4fa92d548a31a80720
Alexis LOURS <alexislours@protonmail.com>
2023-08-12 21:48:53 +0200

    feat: add content to feed.xml

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -3,6 +3,10 @@ import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../constants";
 import type { APIContext } from "astro";
 
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
+
 export async function get(context: APIContext & { site: URL }) {
   const posts = (await getCollection("blog")).sort(
     (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
@@ -13,6 +17,7 @@ export async function get(context: APIContext & { site: URL }) {
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
+      content: sanitizeHtml(parser.render(post.body)),
       link: `/blog/${post.slug}/`,
     })),
   });
