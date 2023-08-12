449552091631e03e475017d5953fd52c53cbe0a8
Alexis LOURS <alexislours@protonmail.com>
2023-08-12 22:42:56 +0200

    feat: add remark/rehype plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
+import remarkToc from "remark-toc";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings/lib";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://alexi.sh",
   compressHTML: true,
+  markdown: {
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: "wrap" }]],
+  },
   build: {
     inlineStylesheets: "auto",
   },
