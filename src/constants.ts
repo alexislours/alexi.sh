bdd1942e19c980c9ca293e409960ce3a0c93a39c
Alexis LOURS <alexislours@protonmail.com>
2023-10-01 04:04:12 +0200

    feat: add pagination component

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const SITE_TITLE = "Alexis LOURS";
 export const SITE_DESCRIPTION = "Full-Stack developer from France.";
 export const SIDEBAR_LINKS: { title: string; href: string }[] = [
   { title: "Home", href: "/" },
-  { title: "Photos", href: "/photos" },
+  { title: "Photos", href: "/photos/1" },
   { title: "Blog", href: "/blog" },
   { title: "Projects", href: "/projects" },
   { title: "Finds", href: "/finds" },
