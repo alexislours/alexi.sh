df9e5e80ceee1d7f0ce40a6983d1edc501c44668
Alexis LOURS <alexislours@protonmail.com>
2023-11-16 00:03:33 +0100

    chore: update to alexislours.com

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const SITE_TITLE = "Alexis LOURS";
 export const SITE_DESCRIPTION = "Full-Stack developer from France.";
 export const SIDEBAR_LINKS: { title: string; href: string }[] = [
   { title: "Home", href: "/" },
-  { title: "Photos", href: "https://photos.alexi.sh" },
+  { title: "Photos", href: "https://alexislours.com" },
   { title: "Blog", href: "/blog" },
   { title: "Projects", href: "/projects" },
   { title: "Finds", href: "/finds" },
